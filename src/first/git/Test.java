a0f0f651bf8e9afef43a86e1b7d8cc5c639d3268
Hussain Rezai <hussain.rasa111@gmail.com>
2022-11-09 16:40:38 -0500

    For checking

---
+++ src/first/git/Test.java
@@ -0,0 +1,10 @@
+package first.git;
+
+public class Test {
+
+	public static void main(String [] args) {
+		System.out.println("This is just for test!!!");
+		System.out.println("This is for pushing to the remote repository!");
+		System.out.println("This is for checking if the code is pushed to the remote repo!");
+	}
+}
