04aebdf53636ed887612c0013884c49f68737e49
Hussain Rezai <hussain.rasa111@gmail.com>
2022-11-09 16:53:11 -0500

    check!

--- src/first/git/SecondClass.java
+++ src/first/git/SecondClass.java
@@ -5,4 +5,8 @@ public class SecondClass {
 	public static void main(String[] args) {
 		System.out.println("hi, how are You?");
 	}
+	
+	public void pushToRepo() {
+		System.out.println("is it pushed to the remote repository?");
+	}
 }
